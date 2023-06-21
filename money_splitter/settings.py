d8718a8f34c13af773e2170ed23040a62d77229c
ShauryaSingh02 <137161534+ShauryaSingh02@users.noreply.github.com>
2023-06-21 12:34:34 +0530

    Add files via upload

--- money_splitter/settings.py
+++ money_splitter/settings.py
@@ -127,7 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+
+import os
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
